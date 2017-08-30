5769292c4241c487c87479ebff73994e0ac24d11
Justin Yoo <justin.yoo@aliencube.com>
2017-08-31 00:13:57 +1000

    Add sample WebJob console app

---
+++ src/WebJob.Host.Autofac/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+using Microsoft.Azure.WebJobs;
+
+namespace WebJob.Host.Autofac
+{
+    /// <summary>
+    /// This represents the function entity for Azure WebJob.
+    /// </summary>
+    public class Functions
+    {
+        /// <summary>
+        /// Processes the incoming queue message.
+        /// </summary>
+        /// <param name="message">Queue message.</param>
+        /// <param name="log">Log writer.</param>
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+    }
+}
---
+++ src/WebJob.Host.Autofac/WebJobModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace WebJob.Host.Autofac
+{
+    /// <summary>
+    /// This represents the Autofac module entity.
+    /// </summary>
+    public class WebJobModule : Module
+    {
+        /// <inheritdoc />
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Functions>().AsSelf().InstancePerDependency();
+        }
+    }
+}
